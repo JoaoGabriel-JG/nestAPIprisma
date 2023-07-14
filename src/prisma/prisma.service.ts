f7c418fe282f5acea39590a3d6e0f824718e0165
JoaoGabriel-JG <joao.nitsus@gmail.com>
2023-07-14 16:19:54 -0300

    att

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,16 @@
-import {Injectable} from "@nestjs/common";
+import {INestApplication, Injectable, OnModuleInit} from "@nestjs/common";
+import {PrismaClient} from "@prisma/client";
+import * as process from "process";
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    async onModuleInit(){
+        await this.$connect()
+    }
+
+    async enabledShutDownHooks(app: INestApplication) {
+        process.on('beforeExit', async () => {
+            await app.close()
+        })
+    }
+}
